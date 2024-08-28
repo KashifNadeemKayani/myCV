61d22e008f1b790816851b44ee78b7f5f7d789c8
KashifNadeemKayani <KAYANIKASHIF970@GMAIL.COM>
2024-08-28 19:52:22 +0500

    linkedin added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWhatsapp, faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faWhatsapp, faInstagram, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope, faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 function Footer() {
@@ -13,6 +13,9 @@ function Footer() {
       <a href="mailto:kashifnadeem898@gmail.com" className="text-white mx-3" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faEnvelope} size="2x" />
         </a>
+        <a href="https://www.linkedin.com/in/kashif-nadeem-kayani-060ab1314" className="text-white mx-3" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
        
       <a href="https://wa.me/+923343288223" className="text-white mx-3" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faWhatsapp} size="2x" />
@@ -23,9 +26,7 @@ function Footer() {
         <a href="https://www.facebook.com/profile.php?id=100069567382242&mibextid=ZbWKwL" className="text-white mx-3" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faFacebook} size="2x" />
         </a>
-        <a href="https://goo.gl/maps/BzJzjLMSfBwq3wvN9" className="text-white mx-3" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faMapMarkerAlt} size="2x" />
-        </a>
+        
      
       </div>
     </footer>
