0970d76ce2af824d31734a60c4526585fc5f67eb
Kashif Nadeem Kayani <kayanikashif970@gmail.com>
2025-04-06 12:28:08 +0500

    updated transcript and pic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from "./Header.module.css";
-import pic from './Pic.jpg'
+import pic from './Picc.jpg'
 
 function Header() {
   return (
     <header className={`bg-dark text-white text-center py-4 ${styles.header}`}>
       <div className="d-flex flex-column align-items-center">
-        <img 
-          src= {pic}
-          alt="Kashif Nadeem Kayani" 
-          className={`rounded-circle ${styles.profileImage}`} 
+        <img
+          src={pic}
+          alt="Kashif Nadeem Kayani"
+          className={`rounded-circle ${styles.profileImage}`}
         />
         <h1 className="mt-3">Kashif Nadeem Kayani</h1>
         <p className="mt-2">Driven to innovate and eager to make an impact.</p>
--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -4,25 +4,25 @@ import styles from './Languages.module.css';
 function Languages() {
   return (
     <section className="my-5">
-      <h2 className={`text-center ${styles.language}`}>Languages</h2> 
+      <h2 className={`text-center ${styles.language}`}>Languages</h2>
       <div className={styles.skillsContainer}>
         <ul className="list-group list-group-flush text-center">
-          
+
           <li className={`list-group-item ${styles.listItem}`}>Urdu</li>
           <div className={`${styles.progressBarContainer} progress`} role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
             <div className={`progress-bar progress-bar-striped progress-bar-animated ${styles.progressBar}`} style={{ width: '100%' }}></div>
           </div>
 
           <li className={`list-group-item ${styles.listItem}`}>English</li>
-          <div className={`${styles.progressBarContainer} progress`} role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
-            <div className={`progress-bar progress-bar-striped progress-bar-animated ${styles.progressBar}`} style={{ width: '85%' }}></div>
+          <div className={`${styles.progressBarContainer} progress`} role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">
+            <div className={`progress-bar progress-bar-striped progress-bar-animated ${styles.progressBar}`} style={{ width: '95%' }}></div>
           </div>
 
           <li className={`list-group-item ${styles.listItem}`}>Punjabi</li>
           <div className={`${styles.progressBarContainer} progress`} role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
             <div className={`progress-bar progress-bar-striped progress-bar-animated ${styles.progressBar}`} style={{ width: '70%' }}></div>
           </div>
-          
+
         </ul>
       </div>
     </section>
