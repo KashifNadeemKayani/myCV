b5dea2bc7751721c671c8d5e9e96e6c1ff64f6a6
Kashif <kayanikashif970@gmail.com>
2026-01-31 17:11:59 +0500

    pic changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,8 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from "./Header.module.css";
-import pic from './Pic/picccc.jpg';
+// import pic from './Pic/picccc.jpg';
+import pic from './Pic/Pic.png';
 
 function Header() {
   const [isZoomed, setIsZoomed] = useState(false);
